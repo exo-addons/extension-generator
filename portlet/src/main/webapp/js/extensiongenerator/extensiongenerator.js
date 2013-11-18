a5d89939579173dbe6392c8ad35ffbfe49871c23
boubaker <bkhanfir@exoplatform.com>
2013-11-18 20:10:08 +0100

    Add Source code

---
+++ portlet/src/main/webapp/js/extensiongenerator/extensiongenerator.js
@@ -0,0 +1,101 @@
+(function($) {
+	$('.title-commands #expandAllTree').on("click", function() {
+		$('.uiTreeExplorer .tree-checkbox').each(function() {
+			this.checked = true;
+		});
+	});
+	$('.title-commands #collapseAllTree').on("click", function() {
+		$('.uiTreeExplorer .tree-checkbox').each(function() {
+			this.checked = false;
+		});
+	});
+	$('.title-commands #controlAll').on("change", function() {
+		var checked = $(this).attr("checked");
+		$('.uiTreeExplorer .list-checkbox.parent').each(function() {
+			this.checked = checked;
+			fireCheckBoxChange(this);
+		});
+		$('#exportImportForm').jzLoad(
+				"ExtensionGeneratorController.selectResources()", {
+					"path" : "",
+					"checked" : ""
+				});
+	});
+
+	$(document).ready(function() {
+		$.fn.DataTable = jQuery.fn.dataTable;
+		$.fn.dataTable = jQuery.fn.dataTable;
+		$.fn.dataTableSettings = jQuery.fn.dataTable.settings;
+		$.fn.dataTableExt = jQuery.fn.dataTable.ext;
+
+		$('.tree_datatable').each(function() {
+			$(this).dataTable({
+				"bSort" : false,
+				"aaSorting" : [],
+				"aoColumns" : [ {
+					"bSortable" : false
+				} ],
+				"bFilter" : true,
+				"bPaginate" : false,
+				"bInfo" : false,
+				"sScrollY" : 252,
+				"oLanguage" : {
+					"sSearch" : "",
+					"oSearch" : false
+				}
+			});
+		});
+	});
+	function fireCheckBoxChange(obj) {
+		if(!obj || !obj.id) {
+			obj = this;
+		}
+		var checkboxId = $(obj).attr("id");
+		var checked = $(obj).attr('checked')
+				&& ($(obj).attr('checked') == 'checked');
+		if (!checked) {
+			checked = false;
+		}
+
+		var childrenChecked = false;
+		var allChildrenChecked = true;
+		$(obj).closest('li').find('.list-checkbox').each(function() {
+			childrenChecked = childrenChecked || this.checked;
+			allChildrenChecked = allChildrenChecked && this.checked;
+		});
+
+		$(obj).closest('ul').closest('li').children('.list-checkbox')
+				.each(
+						function() {
+							this.checked = allChildrenChecked;
+							this.indeterminate = childrenChecked
+									&& !allChildrenChecked;
+						});
+
+		// Select all sub checkboxes
+		if ($(obj).hasClass("parent")) {
+			$(obj).closest('.node').find('.list-checkbox').each(function() {
+				this.checked = checked;
+			});
+		} else if ($(obj).hasClass("leaf")) {
+			var childrenChecked = false;
+			var allChildrenChecked = true;
+			$(obj).closest('.node').find('.list-checkbox').each(function() {
+				childrenChecked = childrenChecked || this.checked;
+				allChildrenChecked = allChildrenChecked && this.checked;
+			});
+			$(obj).closest('.nodeGroup').closest('.node').find(
+					'input[type="checkbox"].parent').each(function() {
+				this.checked = allChildrenChecked;
+				this.indeterminate = childrenChecked && !allChildrenChecked;
+			});
+		}
+		$('#exportImportForm').jzLoad(
+				"ExtensionGeneratorController.selectResources()", {
+					"path" : checkboxId,
+					"checked" : checked
+				});
+	}
+	$('#extension-genrator-portlet .list-checkbox').on("change",
+			fireCheckBoxChange);
+})($);
